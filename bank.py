f5a7e071f4cbf78f89dde79cc0615858b0cea764
Symbat2324 <bagachieva1@gmail.com>
2025-02-05 14:17:39 +0600

    Initial commit

---
+++ bank.py
@@ -0,0 +1,10 @@
+class Amount:
+    def __init__(self, amount: float, timestamp: detetime, transaction_type: str):
+        self.amount = amount
+        self.timestamp = timestamp
+        self.transaction_type = transaction_type
+    def __str__(self):
+        return f"{self.timestamp} - {self.transaction_type}: $ {self.amount: .2f}"
+
+class PersonalAccount:
+    def __init__(self, account_number: int, aacoount_holder: str):
