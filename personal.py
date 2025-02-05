ff9edae26ea83d9708289bde6eff739305b3fa08
Symbat2324 <bagachieva1@gmail.com>
2025-02-05 15:23:16 +0600

    person

--- personal.py
+++ personal.py
@@ -28,4 +28,21 @@ class PersonalAccount:
         else:
             for transaction in self.transaction:
                 print(transaction)
-
+    def get_balance(self):
+        return self.balance
+    def get_account_number(self):
+        return self.account_number
+    def set_account_number(self, account_number: int):
+        self.account_number = account_number
+    def get_account_holder(self):
+        return self.account_holder
+    def set_account_holder(self, account_holder: str):
+        self.account_holder = account_holder
+    def __str__(self):
+        return f'Account number{self.account_number}, Holder: {self.account_holder}, Balance: ${self.balance: .2f}"
+    def __add__(self, amount: float):
+        self.deposit(amount)
+        return self
+    def __sub__(self, amount: float):
+        self.withdraw(amount)
+        return self
